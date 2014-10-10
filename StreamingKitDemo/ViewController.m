4fa7f131a252e74d42081917c2931d2270d5db8f
Goodman <jarryd999@oscr-mz159.catnet.arizona.edu>
2014-10-09 19:10:50 -0700

    Added player functionality and required methods.  Play song when first view loads.

--- StreamingKitDemo/ViewController.m
+++ StreamingKitDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <StreamingKit/STKAudioPlayer.h>
 
-@interface ViewController ()
+
+@interface ViewController () <STKAudioPlayerDelegate>
+
+@property (nonatomic, strong) STKAudioPlayer *audioplayer;
 
 @end
 
@@ -16,12 +20,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.audioplayer = [[STKAudioPlayer alloc] init];
+    
+    [self.audioplayer play:@"http://slseduction.parseapp.com/music/SimpleThings-Single.mp3"];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - STKAudioPlayerDelegate Protocol Reference
+
+- (void) audioPlayer:(STKAudioPlayer *)audioPlayer didStartPlayingQueueItemId:(NSObject *)queueItemId
+{
+    NSLog(@"did Start Playing song");
+}
+
+- (void) audioPlayer:(STKAudioPlayer *)audioPlayer didFinishBufferingSourceWithQueueItemId:(NSObject *)queueItemId
+{
+    NSLog(@"Did finish buffering");
+}
+
+- (void) audioPlayer:(STKAudioPlayer *)audioPlayer stateChanged:(STKAudioPlayerState)state previousState:(STKAudioPlayerState)previousState
+{
+    NSLog(@"State changed");
+}
+
+- (void) audioPlayer:(STKAudioPlayer *)audioPlayer didFinishPlayingQueueItemId:(NSObject *)queueItemId withReason:(STKAudioPlayerStopReason)stopReason andProgress:(double)progress andDuration:(double)duration
+{
+    NSLog(@"Finished playing");
+}
+
+- (void) audioPlayer:(STKAudioPlayer *)audioPlayer unexpectedError:(STKAudioPlayerErrorCode)errorCode
+{
+    (NSLog(@"Error"));
 }
 
 @end
