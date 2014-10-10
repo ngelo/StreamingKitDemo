ca8c80ad97efd50e30fc172e448f49dee573468d
Nikolas Gelo <nikolas.gelo@gmail.com>
2014-10-09 18:53:38 -0700

    Initial Commit

---
+++ StreamingKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StreamingKitDemo
+//
+//  Created by Nikolas Gelo on 10/9/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StreamingKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StreamingKitDemo
+//
+//  Created by Nikolas Gelo on 10/9/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
