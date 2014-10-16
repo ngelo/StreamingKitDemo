851ec2dd4293ca5d39b6ac94ffc6e69afe9f4411
Nikolas Gelo <nikolas.gelo@gmail.com>
2014-10-15 19:08:30 -0700

    STKAudioPlayer subclass initial commit.

---
+++ StreamingKitDemo/AudioPlayer.h
@@ -0,0 +1,13 @@
+//
+//  AudioPlayer.h
+//  StreamingKitDemo
+//
+//  Created by Nikolas Gelo on 10/15/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+#import "STKAudioPlayer.h"
+
+@interface AudioPlayer : STKAudioPlayer
+
+@end
---
+++ StreamingKitDemo/AudioPlayer.m
@@ -0,0 +1,13 @@
+//
+//  AudioPlayer.m
+//  StreamingKitDemo
+//
+//  Created by Nikolas Gelo on 10/15/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+#import "AudioPlayer.h"
+
+@implementation AudioPlayer
+
+@end
